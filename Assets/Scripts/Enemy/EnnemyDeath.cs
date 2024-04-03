6a6a699a2273bcbe8c489cdddc3170235d890823
JaquyLaTortue <93046036+JaquyLaTortue@users.noreply.github.com>
2024-04-03 12:07:16 +0200

    Try death effect on ennemy

---
+++ Assets/Scripts/Enemy/EnnemyDeath.cs
@@ -0,0 +1,43 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class EnnemyDeath : MonoBehaviour
+{
+    public bool isDead = false;
+
+    private bool alreadyDead = false;
+
+    [SerializeField]
+    private EnnemyMain _ennemyMain;
+
+    private Rigidbody _rb;
+
+    [SerializeField]
+    private float _bumpForce = 10f;
+
+    [SerializeField]
+    private GameObject _deathEffect;
+
+    private void Start()
+    {
+        _rb = _ennemyMain._ennemyMovement._rb;
+    }
+
+    private void Update()
+    {
+        if (isDead && !alreadyDead)
+        {
+            Die();
+            alreadyDead = true;
+        }
+    }
+
+    public void Die()
+    {
+        _deathEffect.SetActive(true);
+        _rb.AddForce(Vector3.up * _bumpForce, ForceMode.Impulse);
+        _ennemyMain._ennemyMovement._collider.enabled = false;
+        DOTween.Sequence(_ennemyMain.transform.DORotate(new Vector3(0, 0, 180), 3f))
+            .Append(_ennemyMain.transform.DOPunchScale(Vector3.up, 3f, 2));
+    }
+}
