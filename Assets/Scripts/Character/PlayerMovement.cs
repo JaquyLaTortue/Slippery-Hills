435125e0ed7f480c0dffb97af9cbb353055e76e9
MrSoda36 <trystan.perello@outlook.fr>
2024-04-03 15:05:05 +0200

    Working PlayerMovement

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -8,41 +8,56 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float _jumpForce = 5f;
     [field:SerializeField]
-    public bool IsCrouching { get; private set; } = false;
+    public bool IsSliding { get; private set; } = false;
+    private bool _canJump = true;
     public Vector3 MoveDirection;
 
     private Rigidbody _rigidbody;
+    [field:SerializeField]
+    public PhysicMaterial PhysicMat { get; private set; }
 
     private void Start() {
         _rigidbody = GetComponent<Rigidbody>();
+        PhysicMat = GetComponent<CapsuleCollider>().material;
     }
 
     private void FixedUpdate() {
-        _rigidbody.velocity = new Vector3(MoveDirection.x * Time.deltaTime, _rigidbody.velocity.y, _rigidbody.velocity.z);
+        if(MoveDirection.x != 0) {
+            if (!IsSliding) {
+                _rigidbody.velocity = new Vector3(MoveDirection.x * _maxSpeed * Time.deltaTime, _rigidbody.velocity.y, _rigidbody.velocity.z);
+            }  
+        } 
     }
 
     public void Move(InputAction.CallbackContext ctx) {
-        if (IsCrouching) {
-            MoveDirection = new Vector3(ctx.ReadValue<Vector2>().x * _maxSpeed / 2, 0, 0);
-        }
-        else {
-            MoveDirection = new Vector3(ctx.ReadValue<Vector2>().x * _maxSpeed, 0, 0);
-        }
-
+        MoveDirection = new Vector3(ctx.ReadValue<Vector2>().x, 0, 0);
     }
 
     public void Jump(InputAction.CallbackContext ctx) {
+        if (!ctx.started || !_canJump) return;
+        _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        _canJump = false;
+    }
+
+    public void Slide(InputAction.CallbackContext ctx) {
         if (ctx.started) {
-            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            IsSliding = true;
+            PhysicMat.dynamicFriction = 1f;
+            PhysicMat.staticFriction = 1f;
+            PhysicMat.frictionCombine = PhysicMaterialCombine.Minimum;
+        }
+        if (ctx.canceled) {
+            IsSliding = false;
+            PhysicMat.dynamicFriction = 2f;
+            PhysicMat.staticFriction = 2f;
+            PhysicMat.frictionCombine = PhysicMaterialCombine.Average;
         }
     }
 
-    public void Crouch(InputAction.CallbackContext ctx) {
-        if (ctx.started) {
-            IsCrouching = true;
-        }
-        if(ctx.canceled) {
-            IsCrouching = false;
+    //Allow the player to jump again when he touches the ground
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            _canJump = true;
         }
     }
 }
